2b8fcba7ae5d011cc00f47b224a2311f6c918bae
sk1001 <sheilafleming81@gmail.com>
2022-07-14 12:32:16 +0100

    New button for upcoming movies page.

--- src/pages/upComing.js
+++ src/pages/upComing.js
@@ -41,7 +41,8 @@ import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
 import { getUpcomingMovies } from '../api/tmdb-api'
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+// import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import PlaylistAddIcon from '../components/cardIcons/addToPlaylist'
 
 const UpComing = (props) => {
   const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
@@ -67,7 +68,7 @@ const UpComing = (props) => {
       title="Discover UpcomingMovies"
       movies={upcomingMovies}
       action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />
+        return <PlaylistAddIcon movie={movie} />
       }}
     />
 );
