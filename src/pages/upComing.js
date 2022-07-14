57c98cb4cc8f5835adc1ced4f9708b8c769865c6
sk1001 <sheilafleming81@gmail.com>
2022-07-14 12:18:51 +0100

    Fix upcoming movies page.

--- src/pages/upComing.js
+++ src/pages/upComing.js
@@ -1,34 +1,76 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage'
-import { getUpcomingMovies } from "../api/tmdb-api";
+// import React, { useState, useEffect } from "react";
+// import PageTemplate from '../components/templateMovieListPage'
+// import { getUpcomingMovies } from "../api/tmdb-api";
+
+
+// const UpComing = (props) => {
+//   const [movies, setMovies] = useState([]);
+//   const favourites = movies.filter(m => m.favourite)
+//   localStorage.setItem('favourites', JSON.stringify(favourites))
+
+//   // const addToFavourites = (movieId) => {
+//   //   const updatedMovies = movies.map((m) =>
+//   //     m.id === movieId ? { ...m, favourite: true } : m
+//   //   );
+//   //   setMovies(updatedMovies);
+//   // };
+
+// const addToFavourites = () => null;
+
+//   useEffect(() => {
+//     getUpcomingMovies().then(movies => {
+//       setMovies(movies);
+//     });
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
+
+//   return (
+//     <PageTemplate
+//       title='Upcoming Movies'
+//       movies={movies}
+//       selectFavourite={addToFavourites}
+//     />
+//   );
+// };
+// export default UpComing;
+
+
+
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from 'react-query'
+import Spinner from '../components/spinner'
+import { getUpcomingMovies } from '../api/tmdb-api'
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
 
 const UpComing = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favourites = movies.filter(m => m.favourite)
-  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
 
-//   const addToFavourites = (movieId) => {
-//     const updatedMovies = movies.map((m) =>
-//       m.id === movieId ? { ...m, favourite: true } : m
-//     );
-//     setMovies(updatedMovies);
-//   };
+  if (isLoading) {
+    return <Spinner />
+  }
 
-const addToFavourites = () => null;
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const upcomingMovies = data.results;
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // These three lines are redundant; we will replace them laterg.
+  //  const favourites = movies.filter(m => m.favouurite)
+  //  localStorage.setItem('favourites', JSON.stringify(favourites))
+  //const addToFavourites = (movieId) => true 
 
+
+  
   return (
     <PageTemplate
-      title='Upcoming Movies'
-      movies={movies}
-      selectFavourite={addToFavourites}
+      title="Discover UpcomingMovies"
+      movies={upcomingMovies}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
     />
-  );
+);
 };
+
 export default UpComing;
