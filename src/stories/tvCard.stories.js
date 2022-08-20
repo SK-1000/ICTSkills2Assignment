08705753e346e63c117b3c2cacbf31c6af8d51d2
sk1001 <sheilafleming81@gmail.com>
2022-08-20 23:09:00 +0100

    updated tv stories

--- src/stories/tvCard.stories.js
+++ src/stories/tvCard.stories.js
@@ -1,16 +1,52 @@
+// import React from "react";
+// import TvCard from "../components/tvCard";
+// import SampleTv from "./sampleTvData";
+
+// export default {
+//   title: "TV Show Page/TvCard",
+//   component: TvCard,
+// };
+
+// export const Basic = () => {
+//   return (
+//     <TvCard
+//       tv={SampleTv}
+//     />
+//   );
+// };
+// Basic.storyName = "Default";
+
+// export const Exceptional = () => {
+//   const sampleNoPoster = { ...SampleTv, poster_path: undefined };
+//   return (
+//     <TvCard
+//       tv={sampleNoPoster}
+//     />
+//   );
+// };
+// Exceptional.storyName = "exception";
+
 import React from "react";
 import TvCard from "../components/tvCard";
 import SampleTv from "./sampleTvData";
+import { MemoryRouter } from "react-router";
+import { action } from "@storybook/addon-actions";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavourites";
 
 export default {
-  title: "TV Show Page/TvCard",
+  title: "Tv Show Page/TvCard",
   component: TvCard,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+  ],
 };
 
 export const Basic = () => {
   return (
     <TvCard
       tv={SampleTv}
+      action={(tv) => <AddToFavoritesIcon tv={tv} />}
+      taging={(tv) => null}
     />
   );
 };
@@ -21,6 +57,8 @@ export const Exceptional = () => {
   return (
     <TvCard
       tv={sampleNoPoster}
+      action={(tv) => <AddToFavoritesIcon tv={tv} />}
+      taging={(tv) => null}
     />
   );
 };
